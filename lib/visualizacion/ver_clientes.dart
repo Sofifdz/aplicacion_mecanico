24327ae1f9e89bf0a13f8a3f08a56fa2f8cf97a7
sf <chofi.sofiamarianana@gmail.com>
2023-05-01 14:56:10 -0600

    listador_ver_clientes

--- lib/visualizacion/ver_clientes.dart
+++ lib/visualizacion/ver_clientes.dart
@@ -15,10 +15,9 @@ class Ver_clientes extends StatefulWidget {
 }
 
 class Ver_clientesState extends State<Ver_clientes> {
-  // var bb = Hive.openBox('clientes');
-  //var clientesbox = Hive.box('clientes').values.toList();
+  var box = Hive.box('clientes');
+  var clientesbox = Hive.box('clientes').values.toList();
   List<dynamic> clientes = [];
-  //var boxclientes = Hive.box('clientes');
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +36,134 @@ class Ver_clientesState extends State<Ver_clientes> {
           ),
         ],
       ),
+      body: ListView.builder(
+          itemCount: box.length,
+          itemBuilder: (context, index) {
+            var b = clientesbox[index];
+
+            return Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(20),
+                elevation: 3,
+                child: Row(children: <Widget>[
+                  SizedBox(
+                    height: 90,
+                  ),
+                  Expanded(
+                    child: Center(
+                        child: Text(
+                      clientesbox[index].nombre,
+                      style: GoogleFonts.hanuman(
+                        fontSize: 25,
+                      ),
+                    )),
+                  ),
+                  PopupMenuButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(8.0),
+                        bottomRight: Radius.circular(8.0),
+                        topLeft: Radius.circular(8.0),
+                        topRight: Radius.circular(8.0),
+                      ),
+                    ),
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        value: 1,
+                        child: Text("Mas detalles"),
+                      ),
+                      PopupMenuItem(
+                        value: 2,
+                        child: Text("Agregar Carro"),
+                      ),
+                      PopupMenuItem(
+                        value: 3,
+                        child: Text("Eliminar Cliente"),
+                      ),
+                    ],
+                    initialValue: 0,
+                    //  onCanceled: () {
+                    // print("You have canceled the menu.");
+                    //  },
+                    onSelected: (value) {
+                      if (value == 1) {
+                        /*  Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Ver_DatosCliente();
+                            },
+                          ),
+                        );*/
+                      }
+                      if (value == 2) {}
+                      if (value == 3) {
+                        showDialog(
+                            context: context,
+                            builder: (buildcontext) {
+                              return AlertDialog(
+                                title: Text('Eliminar Cliente'),
+                                content: const Text(
+                                    "¿Seguro que quieres eliminar este cliente? (se eliminaran los carros agregados)"),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Cancelar'),
+                                  ),
+                                  TextButton(
+                                      onPressed: () {
+                                        Hive.box('clientes').deleteAt(index);
+                                        Navigator.pop(context);
+                                        setState(() {});
+                                      },
+                                      child: Text('Aceptar'))
+                                ],
+                              );
+                            });
+                      }
+                    },
+                    icon: Icon(Icons.more_vert_outlined),
+                  )
+                ]));
+          }),
       floatingActionButton: BotonEspeed(),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
     );
   }
+
+  /* void _showAlertDialog(index) {
+    showDialog(
+        context: context,
+        builder: (buildcontext) {
+          return AlertDialog(
+            content: Text(
+                "¿Seguro que quieres eliminar este cliente? (se eliminaran los carros agregados)"),
+            actions: <Widget>[
+              FilledButton(
+                child: Text(
+                  "Cancelar",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FilledButton(
+                child: Text(
+                  "Aceptar",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Hive.box('clientes').deleteAll(index);
+                  Navigator.pop(context);
+                  setState(() {});
+                },
+              )
+            ],
+          );
+        });
+  }*/
 }
